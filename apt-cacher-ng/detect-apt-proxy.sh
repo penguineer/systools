0b563e61054caa419cd51242a07028df8cb8641b
Stefan Haun <tux@netz39.de>
2019-11-04 01:20:44 +0100

    Shorten detect script even more
    according to https://jaduks.livejournal.com/7934.html

--- apt-cacher-ng/detect-apt-proxy.sh
+++ apt-cacher-ng/detect-apt-proxy.sh
@@ -7,12 +7,8 @@
 # See https://wiki.debian.org/AptCacherNg#Clients for details on
 # return values
 
-if [ -z $APT_PROXY_PORT ] ; then
-  APT_PROXY_PORT=3142
-fi
-if [ -z "${HOST_IP}" ]; then
-  HOST_IP="apt-proxy"
-fi
+APT_PROXY_PORT=${APT_PROXY_PORT:-3142}
+HOST_IP=${HOST_IP:-"apt-proxy"}
 
 timeout 1 cat < /dev/null > /dev/tcp/${HOST_IP}/${APT_PROXY_PORT} 2> /dev/null
 if [ $? -eq 0 ]; then
