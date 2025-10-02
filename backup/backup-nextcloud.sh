4e711060d36ce6ea8a93db98792f29346655f186
Stefan Haun <mail@tuxathome.de>
2025-10-02 13:24:10 +0200

    Add note regarding non-terminal runtime environment

--- backup/backup-nextcloud.sh
+++ backup/backup-nextcloud.sh
@@ -12,6 +12,9 @@
 # For the process see
 # https://docs.nextcloud.com/server/13/admin_manual/maintenance/backup.html
 #
+# Note: This script is intended to be run from a cron job and therefore does not
+# have access to a terminal. All output is sent to stdout/stderr.
+#
 # Author: Stefan Haun <tux@netz39.de>
 #
 # SPDX-License-Identifier: MIT
